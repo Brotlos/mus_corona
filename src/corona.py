8c0785d94991d2616da3a8dc20f81776cb74bd41
Temar <holgerbramstedt@googlemail.com>
2020-08-19 13:43:05 +0200

    Grundstruktur

---
+++ src/corona.py
@@ -0,0 +1,41 @@
+import simpy
+
+
+class Population (object):
+
+    #region Parameter
+    #TODO Parameter für Zeitabschnitt
+    #endregion
+
+    #TODO RESS
+
+    def __init__(self, env, s_class_cap, i_class_cap = 0, r_class_cap = 0):
+        self.env = env
+        # Start the run process everytime an instance is created.
+        self.action = env.process(self.run())
+    
+    def event_setup (self, events):
+        #TODO übergeben events registrieren
+        pass
+
+    def run(self):
+        while True:
+
+            pass
+            #Die Population existiert und infiziert sich gemäß der voreingestellten Parameter
+
+            #TODO Die Ressourcen müssen nach den Parametern berrechnet werden.
+            #Für jeden Zeitabschnitt
+
+            #TODO Check for new events
+
+
+def rate_changing_event(self, env, population):
+    #TODO event auslösen für population
+    pass
+
+
+env = simpy.Environment()
+pop_germany = Population(env, 80000000)
+
+pop_germany.run()
